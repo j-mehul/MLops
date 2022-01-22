8d060cf9911c4759e9c14884e686af35089e7905
Mehul Jain <j.13mehul@gmail.com>
2022-01-22 23:37:44 +0530

    app templtes added

---
+++ tests/test_config.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+class NotInRange(Exception):
+    def __init__(self, message= "Value not is range"):
+        self.message = message
+        super().__init__(self.message )
+
+
+def test_generic():
+    a=2
+
+    with pytest.raises(NotInRange):
+        if a not in range(10,20):
+            raise NotInRange
+
+    
